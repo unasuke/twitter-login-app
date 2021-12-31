9d883be73aa2636b5a84de019c53f60e316ad31d
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-12-31 21:39:09 +0900

    Create logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,11 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   private
 
   def auth_hash
