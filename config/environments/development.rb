b39b5fbb5139b33014ce17af414f7a7be7196471
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-12-31 17:52:28 +0900

    bin/rails app:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
@@ -61,6 +64,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.web_console.whitelisted_ips = "0.0.0.0/0"
 end
