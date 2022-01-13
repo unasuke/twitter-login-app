c29ea71e34839c8cb22f937065ebed5673b22abd
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-01-13 20:41:47 +0900

    omniauth-twitter2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,12 @@
+# require 'omniauth/strategies/twitter2'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   # configure do |config|
   #   config.path_prefix = "/"
   # end
 
   # binding.irb
-  provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"], callback_path: '/auth/twitter/callback'
+  provider :twitter2, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"], callback_path: '/auth/twitter2/callback', scope: "tweet.read users.read"
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get] if Rails.env.development?
