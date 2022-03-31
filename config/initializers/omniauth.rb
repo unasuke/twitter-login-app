ce8efeebea817ca7539fca4062fee119f7736973
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-04-01 00:05:03 +0900

    Fix environment variable name

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # end
 
   # binding.irb
-  provider :twitter2, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"], callback_path: '/auth/twitter2/callback', scope: "tweet.read users.read"
+  provider :twitter2, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], callback_path: '/auth/twitter2/callback', scope: "tweet.read users.read"
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get] if Rails.env.development?
