357d3b03383af154690d08b8cace208a0f7372d3
Yusuke Nakamura <yusuke1994525@gmail.com>
2026-03-12 00:24:44 +0900

    Test for confirmed email field

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # end
 
   # binding.irb
-  provider :twitter2, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], callback_path: '/auth/twitter2/callback', scope: "tweet.read users.read"
+  provider :twitter2, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], callback_path: '/auth/twitter2/callback', scope: "tweet.read users.read users.email"
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get] if Rails.env.development?
